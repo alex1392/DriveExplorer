625e36634547b0568bdc5bd8d0251432f9038397
Alex Chuang <1392alex1392@gmail.com>
2020-05-21 16:59:23 +0100

    Modify: itemModel constructor

--- DriveExplorer/ItemModel.cs
+++ DriveExplorer/ItemModel.cs
@@ -1,17 +1,24 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Path = System.IO.Path;
 
 namespace DriveExplorer {
     public class ItemModel {
-        private ItemModel() {
+        public ItemModel(string fullPath) {
+            FullPath = fullPath;
+            var isRoot = IsRoot(fullPath);
+            Type = isRoot ? Types.Drive :
+                            IsFolder(fullPath) ? Types.Folder :
+                                                 GetFileType(fullPath);
+            if (isRoot) {
+                Name = fullPath.Replace(Path.DirectorySeparatorChar, ' ').Trim();
+            } else {
+                if (fullPath.Last() == '\\') {
+                    fullPath = fullPath.Remove(fullPath.Length - 1);
+                }
+                Name = Path.GetFileName(fullPath);
+            }
 
-        }
-        public ItemModel(Types type, string fullPath, bool isRoot = false) {
-            Type = type;
-            FullPath = fullPath ?? throw new ArgumentNullException(nameof(fullPath));
-            Name = isRoot ?
-                fullPath.Replace(Path.DirectorySeparatorChar, ' ').Trim() :
-                Path.GetFileName(fullPath);
         }
 
         public enum Types {
@@ -28,6 +35,42 @@ namespace DriveExplorer {
         public Types Type { get; set; }
         public string Name { get; set; }
         public string FullPath { get; set; }
+
+        private static readonly Dictionary<string, Types> TypesMap = new Dictionary<string, Types>
+        {
+            {".jpg" , Types.IMG},
+            {".jpeg", Types.IMG},
+            {".png" , Types.IMG},
+            {".bmp" , Types.IMG},
+            {".gif" , Types.IMG},
+            {".txt" , Types.TXT},
+            {".doc" , Types.DOC},
+            {".docx", Types.DOC},
+            {".xls" , Types.XLS},
+            {".xlsx", Types.XLS},
+            {".ppt" , Types.PPT},
+            {".pptx", Types.PPT},
+            {".rar" , Types.ZIP},
+            {".zip" , Types.ZIP},
+        };
+        private ItemModel() {
+
+        }
+        private Types GetFileType(string fullPath) {
+            var ext = Path.GetExtension(fullPath).ToLower();
+            if (TypesMap.ContainsKey(ext)) {
+                return TypesMap[ext];
+            }
+            return Types.File;
+        }
+        private bool IsRoot(string fullPath) {
+            var n = fullPath.Length;
+            return fullPath[n - 1] == '\\' && fullPath[n - 2] == ':';
+        }
+        private bool IsFolder(string fullPath) {
+            var ext = Path.GetExtension(fullPath);
+            return string.IsNullOrEmpty(ext);
+        }
 
     }
 
