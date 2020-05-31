135d3b39b91d86a32c8d87453a7488774a3ea9e7
Alex Chuang <1392alex1392@gmail.com>
2020-05-31 16:33:00 +0100

    fix fileImageConverter

--- DriveExplorerTests/Views/Converters/FileImageConverterTests.cs
+++ DriveExplorerTests/Views/Converters/FileImageConverterTests.cs
@@ -8,13 +8,13 @@ using System.Windows.Media.Imaging;
 
 namespace DriveExplorer.Views.Tests {
 	[TestFixture()]
-	public class TypeImageConverterTests {
+	public class FileImageConverterTests {
 		[Test()]
 		public void ConvertTest() {
 			var app = Application.Current; // ensure pack uri scheme
 			var converter = new TypeImageConverter();
-			var actual = (BitmapImage)converter.Convert(ItemTypes.OneDrive, null, null, null);
-			var expected = new BitmapImage(new Uri($"pack://application:,,,/DriveExplorer;component/Resources/onedrive.png"));
+			var actual = (BitmapImage)converter.Convert(new LocalItem(@"C:\"), null, null, null);
+			var expected = new BitmapImage(new Uri($"pack://application:,,,/DriveExplorer;component/Resources/localdrive.png"));
 			Assert.AreEqual(expected.BaseUri, actual.BaseUri);
 		}
 
