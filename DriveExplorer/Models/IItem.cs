3d69e0bb7bde95e5be650938cba3134bbcdf52b1
Alex Chuang <1392alex1392@gmail.com>
2020-05-31 12:59:58 +0100

    update iitem

--- DriveExplorer/Models/IItem.cs
+++ DriveExplorer/Models/IItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace DriveExplorer.Models {
 	public interface IItem {
@@ -8,6 +9,7 @@ namespace DriveExplorer.Models {
 		string FullPath { get; }
 		long? Size { get; }
 		DateTimeOffset? LastModifiedTime { get; }
+		Task DownloadAsync(string localPath);
 		IAsyncEnumerable<IItem> GetChildrenAsync();
 	}
 }
