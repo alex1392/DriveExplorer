517943a42432cd42a4a1edfe48d2bb3235fed451
Alex Chuang <1392alex1392@gmail.com>
2020-06-03 02:59:27 +0100

    add revealWindow

--- DriveExplorer/Views/MainWindow.xaml.cs
+++ DriveExplorer/Views/MainWindow.xaml.cs
@@ -7,13 +7,14 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Linq;
 using Cyc;
+using Cyc.FluentDesign;
 
 namespace DriveExplorer.Views {
 
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window {
+	public partial class MainWindow : RevealWindow {
 
 		#region Private Fields
 
